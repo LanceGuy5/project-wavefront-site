0f5a1b2cc1bf6daad1340b2873319b42c8b6a6e2
LanceGuy5 <lanceforresth@gmail.com>
2025-09-02 23:04:29 -0400

    homepage working well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Header from "@/components/header";
-import { Card } from "@/components/ui/card";
+import Galaxy from "@/components/Galaxy";
 import { ChevronDown } from "lucide-react";
 
 import Image from "next/image";
@@ -24,7 +24,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto text-center w-full">
           <div className="animate-fade-slide-up">
             <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold mb-6">
-              Project Wavefront
+              PROJECT WAVEFRONT
             </h1>
             <p className="text-xl md:text-2xl text-slate-300 mb-12 max-w-4xl mx-auto leading-relaxed">
               The first student-built rotating detonation rocket engine,
@@ -40,10 +40,10 @@ export default function Home() {
 
       {/** Mark I Section (Carousel ?) */}
       <section className="relative z-10 pt-24 pb-16 px-4 sm:px-6 lg:px-8 min-h-screen flex items-end">
-        <div className="absolute inset-x-0 top-0 w-full h-[100vh] bg-black z-[-1]">
+        <div className="absolute inset-x-0 top-0 w-full h-[100vh] bg-black z-[-1] min-h-screen">
           <Image
             src="/home/injector.jpg"
-            className='opacity-60 blur-sm'
+            className="opacity-60 blur-sm [mask-image:linear-gradient(to_right,transparent_0%,black_0%,black_100%,transparent_100%),linear-gradient(to_bottom,transparent_0%,black_0%,black_100%,transparent_100%)]"
             alt="Project Wavefront banner"
             layout="fill"
             objectFit="cover"
@@ -98,56 +98,6 @@ export default function Home() {
           </div>
           {/* Right Column: Image */}
           <div className="relative h-full min-h-screen">
-            <Card className="absolute top-8 left-8 p-6 border-none w-[40vw] h-[40vw] mt-20">
-              <Image
-                src="/home/injector.jpg"
-                alt="Project Wavefront mission image"
-                layout="fill"
-                objectFit="cover"
-                className="rounded-lg border-white border-opacity-50 border"
-              />
-            </Card>
-          </div>
-        </div>
-      </section>
-
-      {/** Sponsors Section */}
-      <section className="relative min-h-screen flex items-end">
-        <div className="grid grid-cols-1 md:grid-cols-2 w-full h-full">
-          {/* Left Column: Text */}
-          <div className="flex flex-col items-start justify-start ml-8 mt-24 pl-2 sm:pl-4 lg:pl-6">
-            <h1 className="text-4xl md:text-6xl lg:text-6xl font-bold mb-4">
-              OUR MISSION
-            </h1>
-
-            <div className="flex flex-col mt-4 space-y-2">
-              <div className="flex flex-row items-center">
-                <Image src='/home/engineering-icon.png' width={70} height={70} alt={""} />
-                <h3 className='text-3xl md:text-4xl lg:text-4xl'>DESIGN</h3>
-              </div>
-              <p className="text-xl md:text-2xl text-slate-300 mb-12 leading-relaxed max-w-xl">
-                To innovate and push the boundaries of
-                rocket propulsion through cutting-edge
-                research and development by an
-                independent undergraduate team.
-              </p>
-            </div>
-
-            <div className="flex flex-col mt-4 space-y-2">
-              <div className="flex flex-row items-center">
-                <Image src='/home/treeicon.png' width={70} height={70} alt={""} />
-                <h3 className='text-3xl md:text-4xl lg:text-4xl'>INTENT</h3>
-              </div>
-              <p className="text-xl md:text-2xl text-slate-300 mb-12 leading-relaxed max-w-xl">
-                To innovate and push the boundaries of
-                rocket propulsion through cutting-edge
-                research and development by an
-                independent undergraduate team.
-              </p>
-            </div>
-          </div>
-          {/* Right Column: Image */}
-          <div className="relative h-full min-h-screen">
             <Image
               src="/home/injector.jpg"
               alt="Project Wavefront mission image"
@@ -158,6 +108,74 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/** Sponsors Section */}
+      <section className="relative min-h-screen flex items-end">
+        <div className="absolute inset-x-0 top-0 w-full h-[100vh] bg-black z-[-1] min-h-screen">
+          <Galaxy
+            mouseRepulsion={false}
+            mouseInteraction={false}
+            density={1.5}
+            glowIntensity={0.2}
+            saturation={0.4}
+            hueShift={240}
+            twinkleIntensity={0.3}
+            rotationSpeed={0.05}
+            repulsionStrength={2}
+            autoCenterRepulsion={0.1}
+            starSpeed={0.2}
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full h-full">
+          {/* Left Column: Text */}
+          <div className="flex flex-col items-start justify-start ml-8 mt-24 pl-2 sm:pl-4 lg:pl-6">
+            <h1 className="text-4xl md:text-6xl lg:text-6xl font-bold mb-4">
+              SPONSORS & PARTNERS
+            </h1>
+            <p className="text-xl md:text-2xl text-slate-300 mb-12 leading-relaxed max-w-xl">
+              To innovate and push the boundaries of
+              rocket propulsion through cutting-edge
+              research and development by an
+              independent undergraduate team.
+            </p>
+            <p className="text-xl md:text-2xl text-slate-300 mb-12 leading-relaxed max-w-xl">
+              To innovate and push the boundaries of
+              rocket propulsion through cutting-edge
+              research and development by an
+              independent undergraduate team.
+            </p>
+            <p className="text-xl md:text-2xl text-slate-300 mb-12 leading-relaxed max-w-xl">
+              To innovate and push the boundaries of
+              rocket propulsion through cutting-edge
+              research and development by an
+              independent undergraduate team.
+            </p>
+
+          </div>
+          {/* Right Column: Sponsor Logos */}
+          <div className="relative h-full min-h-screen">
+            <div className="flex flex-col items-center justify-center h-full w-full p-8 space-y-8">
+              <Image
+                src="/home/sponsors/converge.png"
+                alt="Project Wavefront mission image"
+                width={250}
+                height={250}
+                objectFit="cover"
+                className=""
+              />
+              <Image
+                src="/home/sponsors/nasa.png"
+                alt="Project Wavefront mission image"
+                width={330}
+                height={330}
+                objectFit="cover"
+                className=""
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div >
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,41 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const threshold = 50;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY <= threshold) {
+        setIsScrolled(false);
+      } else if (currentScrollY > lastScrollY) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <nav className="fixed top-0 w-full z-50 bg-background/60 backdrop-blur-sm">
+    <nav
+      className={`fixed top-0 w-full z-50 bg-background/60 backdrop-blur-sm transition-transform duration-300 ${isScrolled ? "-translate-y-full" : "translate-y-0"
+        }`}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 text-lg">
           <Link href="/" className="flex items-center space-x-2">
@@ -20,19 +52,19 @@ export default function Header() {
               href="/our-team"
               className="hover:text-gray-400 hover:scale-105 transition duration-200 transition-scale"
             >
-              Our Team
+              OUR TEAM
             </Link>
             <Link
               href="/contact"
               className="hover:text-gray-400 hover:scale-105 transition duration-200 transition-scale"
             >
-              Contact Us
+              CONTACT US
             </Link>
             <Link
               href="/mark-i"
               className="hover:text-gray-400 hover:scale-105 transition duration-200 transition-scale"
             >
-              Mark I
+              MARK I
             </Link>
           </div>
         </div>
